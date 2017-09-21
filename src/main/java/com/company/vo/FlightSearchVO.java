8aff8396cfad1de87f7d21dae5a8b13af553ed55
anusuyagovindarajan <anusuya.govindarajan@gmail.com>
2017-09-21 08:07:27 +0530

    Removed surge price calculation from FlightSearchVO

--- src/main/java/com/company/vo/FlightSearchVO.java
+++ src/main/java/com/company/vo/FlightSearchVO.java
@@ -1,6 +1,7 @@
 package com.company.vo;
 
 
+import com.company.service.PricingService;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
@@ -11,13 +12,13 @@ import java.util.ArrayList;
 
 public class FlightSearchVO {
 
- Location source;
- Location destination;
- int capacity;
- @DateTimeFormat(pattern = "yyyy-MM-dd")
- LocalDate startDate;
- TravelClassType seatClass;
- ArrayList<FlightVO> availableFlight;
+    Location source;
+    Location destination;
+    int capacity;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    LocalDate startDate;
+    TravelClassType seatClass;
+    ArrayList<FlightVO> availableFlight;
 
     public Location getSource() {
         return source;
@@ -67,86 +68,9 @@ public class FlightSearchVO {
         this.seatClass = seatClass;
     }
 
-    public double surgePrice(FlightVO flight) {
-        if(this.startDate == null)
-          startDate = flight.getStartDate();
-        TravelClassType seatClass = this.seatClass;
-        int capacity = this.capacity;
-        int initialCapacity = flight.getPlaneVO().getSeatTypes().get(seatClass).getInitialCapacity();
-        int currentCapacity = flight.getPlaneVO().getSeatTypes().get(seatClass).getCurrentCapacity();
-        double price = flight.getPlaneVO().getSeatTypes().get(seatClass).getPrice();
-
-        int capacityDiff = (initialCapacity - currentCapacity);
-
-        if (seatClass.equals(TravelClassType.Economy)) {
-            if (capacityDiff >= 0.1 * initialCapacity && capacityDiff <= 0.4 * initialCapacity) {
-                price = price;
-            }
-            else if (capacityDiff > 0.4 * initialCapacity && capacityDiff <= 0.9 * initialCapacity) {
-                price = price + 0.3 * price;
-            }
-            else if (capacityDiff > 0.9 * initialCapacity && capacityDiff <= initialCapacity) {
-                price =  price + 0.6 * price;
-            }
-        }
-
-        String dayOfJourney = startDate.getDayOfWeek().name();
-
-        if (seatClass.equals(TravelClassType.Business)) {
-
-            if (dayOfJourney.equalsIgnoreCase("Monday") || dayOfJourney.equalsIgnoreCase("Friday") || dayOfJourney.equalsIgnoreCase("Sunday")) {
-
-                price = price + 0.4 * price;
-
-            }
-
-        }
-
-        if(seatClass.equals(TravelClassType.First)){
-
-            int daysLeft = Period.between(LocalDate.now(),startDate).getDays();
-            switch (daysLeft){
-                case 10:
-                    price = price;
-                    break;
-                case 9:
-                    price = price + 0.1*price;
-                    break;
-                case 8:
-                    price = price + 0.2*price;
-                    break;
-                case 7:
-                    price = price + 0.3*price;
-                    break;
-                case 6:
-                    price = price + 0.4*price;
-                    break;
-                case 5:
-                    price = price + 0.5*price;
-                    break;
-                case 4:
-                    price = price + 0.6*price;
-                    break;
-                case 3:
-                    price = price + 0.7*price;
-                    break;
-                case 2:
-                    price = price + 0.8*price;
-                    break;
-                case 1:
-                    price = price + 0.9*price;
-                    break;
-                case 0:
-                    price = price + 1*price;
-                    break;
-
-            }
-
-
-        }
-
-        return price * capacity;
-
+    public double getSurgePrice(FlightSearchVO flightSearchVO,FlightVO flightVO) {
+        PricingService pricingService = new PricingService();
+        return pricingService.surgePrice(flightSearchVO,flightVO);
     }
 
 }
